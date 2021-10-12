2a7eb375f66a207d6f96b083cabbd674b4a20a7d
Ricchitech <bhrathrajgowda226@gmail.com>
2021-10-12 11:54:56 +0530

    12102021update

---
+++ addtwonum.py
@@ -0,0 +1,6 @@
+#write a program to add two integers and print the result on the screen, accept the values at runtime
+
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+c=a+b
+print("Sum of two numbers is",c)
